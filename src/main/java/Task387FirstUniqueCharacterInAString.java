8eda5c8dda15b9d022cd65a4d268efeb65383777
Dmitriy Lipenchuk <dmitriy.lipenchuk.01@inbox.ru>
2022-12-28 23:03:01 +0300

    Task 387 done

---
+++ src/main/java/Task387FirstUniqueCharacterInAString.java
@@ -0,0 +1,11 @@
+public class Task387FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.indexOf(s.charAt(i)) == s.lastIndexOf(s.charAt(i))) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
